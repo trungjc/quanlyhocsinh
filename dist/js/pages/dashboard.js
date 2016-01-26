6b29ae3ecdf035d19fe8aa2095f97e5a0f12915b
trungjc <trungnguyenthanh70@gmail.com>
2016-01-26 19:18:30 +0700

    first commit

---
+++ dist/js/pages/dashboard.js
@@ -0,0 +1,57 @@
+/*
+ * Author: Abdullah A Almsaeed
+ * Date: 4 Jan 2014
+ * Description:
+ *      This is a demo file used only for the main dashboard (index.html)
+ **/
+
+$(function () {
+
+  "use strict";
+
+  
+  //jQuery UI sortable for the todo list
+  $(".todo-list").sortable({
+    placeholder: "sort-highlight",
+    handle: ".handle",
+    forcePlaceholderSize: true,
+    zIndex: 999999
+  });
+
+
+  $('.daterange').daterangepicker({
+    ranges: {
+      'Today': [moment(), moment()],
+      'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+      'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+      'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+      'This Month': [moment().startOf('month'), moment().endOf('month')],
+      'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+    },
+    startDate: moment().subtract(29, 'days'),
+    endDate: moment()
+  }, function (start, end) {
+   });
+
+  //The Calender
+  $("#calendar").datepicker();
+
+  //SLIMSCROLL FOR CHAT WIDGET
+  $('#chat-box').slimScroll({
+    height: '250px'
+  });
+
+
+  /* The todo list plugin */
+  $(".todo-list").todolist({
+    onCheck: function (ele) {
+      window.console.log("The element has been checked");
+      return ele;
+    },
+    onUncheck: function (ele) {
+      window.console.log("The element has been unchecked");
+      return ele;
+    }
+  });
+
+});
